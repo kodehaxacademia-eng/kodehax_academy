523afa7ae1d080f939812d2fb5ab2ee6aff8c78a
Dhruvaraj K <dhruvarajk52@gmail.com>
2026-03-18 22:51:20 +0530

    feat: use supabase postgres in production - Add dj-database-url and psycopg2-binary - Conditionally load DB_URL using dj-database-url when PRODUCTION=True - Add PRODUCTION flag to .env

--- kodehax_academy/settings.py
+++ kodehax_academy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,12 +107,19 @@ WSGI_APPLICATION = 'kodehax_academy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+PRODUCTION = os.getenv("PRODUCTION", "False").lower() == "true"
+
+if PRODUCTION:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DB_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
